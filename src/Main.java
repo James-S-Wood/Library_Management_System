136f2a8f340585accb5dc5a9c874625d8d9ba63f
James Wood <james.s.wood@icloud.com>
2023-06-04 06:50:25 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        /* In this application, the library.model package contains the Book and User classes that represent the library's book and user entities. The library.management package contains the Library class responsible for managing books and users. The library.util package provides utility classes BookValidator and UserValidator for validating book and user information.
+
+The LibraryApp class demonstrates the usage of the library system. It creates book and user objects, validates them using the corresponding validators, and adds them to the library using the Library class's methods. Finally, it displays the list of books and users in the library.
+
+This example illustrates a cohesive Java application that utilizes packages, classes, and demonstrates encapsulation, inheritance, and polymorphism in the context of a library management system.
+*/
+
+        }
+    }
